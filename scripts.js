c54473267d07c5fbb19f177fcbc419405819f1a7
Késia Vitória <k.gomeskesia@gmail.com>
2025-02-20 22:21:37 -0300

    Adicionando o menu hamburguer

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function clickMenu() {
+    // menuMobile.style.display = 'block'
+
+    if (navMobile.style.display == 'block') {
+        navMobile.style.display = 'none'
+    } else {
+        navMobile.style.display = 'block'
+    }
+}
